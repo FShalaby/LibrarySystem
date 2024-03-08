f9451c133ec5e4fb7f10ea9fcd71f5bca6600edd
DeltaSonic62 <mali.kermali@outlook.com>
2024-03-08 16:20:20 -0500

    fix: user re-added to csv after login

--- sandbox/src/sandbox/Visitor.java
+++ sandbox/src/sandbox/Visitor.java
@@ -1,18 +1,13 @@
 package sandbox;
 
-public class Visitor extends User{
+public class Visitor extends User {
 
-	
-	public Visitor(String name, String email, String password, String type)
-	{
-		this.name=name;
-		this.email=email;
-		this.pw=password;
-		this.type = type;
-		this.id = this.generateRandomID();
-		this.isVerified = true;
-		 writeUserCsv(this.name, this.email, this.pw, this.id,this.type);
-	}
-	
-
+  public Visitor(String name, String email, String password, String type) {
+    this.name = name;
+    this.email = email;
+    this.pw = password;
+    this.type = type;
+    this.id = generateRandomID();
+    this.isVerified = true;
+  }
 }
