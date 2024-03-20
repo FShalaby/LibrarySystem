fe3e539208bd69aa3864cd7c11a2eb7127748e04
DeltaSonic62 <mali.kermali@outlook.com>
2024-03-20 19:40:13 -0400

    chore: resolve merge conflicts

--- sandbox/src/sandbox/Database.java
+++ sandbox/src/sandbox/Database.java
@@ -282,46 +282,6 @@ public class Database {
     return items;
   }
 
-  public static String fetchNewsletterContent(String urlString) throws IOException {
-    URL url = new URL(urlString);
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod("GET");
-
-    StringBuilder content = new StringBuilder();
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        content.append(line);
-      }
-    } finally {
-      connection.disconnect();
-    }
-
-    return content.toString();
-  }
-
-
-  public static List<Newsletter> getNewsletters() {
-    String filename = getNewslettersCsvFilename();
-
-    ArrayList<Newsletter> items = new ArrayList<>();
-    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        // ignore first line
-        if (line.startsWith("id,") || line.isEmpty()) {
-          continue;
-        }
-
-        items.add(NewsFromCsvLine(line));
-      }
-    } catch (IOException e) {
-      System.err.println(e.getMessage());
-    }
-
-    return items;
-  }
-
   /**
    * Reads the items.csv file and returns an Item with the given ID.
    *
@@ -512,6 +472,88 @@ public class Database {
     return null;
   }
 
+  public static String fetchNewsletterContent(String urlString) throws IOException {
+    URL url = new URL(urlString);
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
+
+    StringBuilder content = new StringBuilder();
+    try (BufferedReader reader =
+        new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        content.append(line);
+      }
+    } finally {
+      connection.disconnect();
+    }
+
+    return content.toString();
+  }
+
+  public static List<Newsletter> getNewsletters() {
+    String filename = getNewslettersCsvFilename();
+
+    ArrayList<Newsletter> items = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        // ignore first line
+        if (line.startsWith("id,") || line.isEmpty()) {
+          continue;
+        }
+
+        items.add(newsletterFromCsvLine(line));
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+    return items;
+  }
+
+  public static List<Newsletter> getUserSubscription(String id) {
+    String filename = getSubscribersCsvFilename();
+
+    ArrayList<Newsletter> subscriptions = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        // ignore first line
+        if (line.startsWith("newsletter_id,") || line.isEmpty()) {
+          continue;
+        }
+
+        String[] parts = line.split(",");
+        if (!Objects.equals(parts[1], id)) continue;
+
+        subscriptions.add(Database.getNews());
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+    return subscriptions;
+  }
+
+  public static Newsletter getNews() {
+    String filename = getNewslettersCsvFilename();
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        if (line.startsWith("id,")) continue;
+
+        Newsletter news = newsletterFromCsvLine(line);
+        return news;
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+    return null;
+  }
+
   // update methods
 
   public static void updateUserVerification(String id, boolean verify) {
@@ -540,9 +582,8 @@ public class Database {
       System.err.println(e.getMessage());
     }
     boolean renamed = tempFile.renameTo(new File(filename));
-    System.out.println("UPDATE_User_Verfication: Rename success? " + renamed);
+    System.out.println("UPDATE_USER_VERIFICATION: Rename success? " + renamed);
   }
-
 
   public void updateItemCopies(String itemID, int adjustment) {
     String filename = getItemsCsvFilename();
@@ -727,6 +768,7 @@ public class Database {
     String path = new File("").getAbsolutePath();
     return path + "/db/newsletters.csv";
   }
+
   private static String getSubscribersCsvFilename() {
     String path = new File("").getAbsolutePath();
     return path + "/db/subscribers.csv";
@@ -766,17 +808,6 @@ public class Database {
    * @param line A line from items.csv
    * @return Item
    */
-  private static Newsletter NewsFromCsvLine(String line) {
-    String[] parts = line.split(",");
-    Newsletter news = new NewsletterProxy();
-    news.id = parts[0];
-    news.name = parts[1];
-    news.url = parts[2];
-    news.fee = Double.parseDouble(parts[3]);
-
-    return news;
-  }
-
   private static Item itemFromCsvLine(String line) {
     String[] parts = line.split(",");
     Item item = new Item();
@@ -813,6 +844,17 @@ public class Database {
     item.isLost = Boolean.parseBoolean(parts[9]);
 
     return item;
+  }
+
+  private static Newsletter newsletterFromCsvLine(String line) {
+    String[] parts = line.split(",");
+    Newsletter news = new NewsletterProxy();
+    news.id = parts[0];
+    news.name = parts[1];
+    news.url = parts[2];
+    news.fee = Double.parseDouble(parts[3]);
+
+    return news;
   }
 
   /**
