567cc6ccd577a15acf71c819f5b2d69aed78abb2
DeltaSonic62 <mali.kermali@outlook.com>
2024-03-22 17:25:39 -0400

    fix: enable/disable items removes purchasable
    closes #65

--- sandbox/src/sandbox/EnableItemCommand.java
+++ sandbox/src/sandbox/EnableItemCommand.java
@@ -1,16 +1,17 @@
 package sandbox;
 
-public class EnableItemCommand implements Command
-{
-	private Item item;
+public class EnableItemCommand implements Command {
+  private Item item;
 
-    public EnableItemCommand(Item item) {
-        this.item = item;
-    }
-	
-	public void execute() {
-		this.item.permission= ItemPermission.Rentable;
-		Database.updateItemPermission(this.item.id, this.item.permission);
-	}
-	
+  public EnableItemCommand(Item item) {
+    this.item = item;
+  }
+
+  public void execute() {
+    this.item.permission =
+        this.item.permission == ItemPermission.Purchasable
+            ? ItemPermission.RentableAndPurchasable
+            : ItemPermission.Rentable;
+    Database.updateItemPermission(this.item.id, this.item.permission);
+  }
 }
